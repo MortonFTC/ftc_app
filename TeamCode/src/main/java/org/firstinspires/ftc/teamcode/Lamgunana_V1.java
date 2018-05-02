70a3ede0fd78aff940f16a86471ba291c218710b
FIRSTMentor <mortonftc@gmail.com>
2018-05-01 19:51:11 -0500

    ZANE AND MILES TEAM BEST TEAM LOL XD

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lamgunana_V1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lamgunana_V1.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp(name = "Lamgunana_V1",group = "mortonElements")
@@ -12,6 +13,11 @@ public class Lamgunana_V1 extends LinearOpMode
 {
     private DcMotor motorLeft;
     private DcMotor motorRight;
+
+    private Servo armServo1;
+    private Servo armServo2;
+    private Servo gripperServo1;
+    private Servo gripperServo2;
 
     @Override
     public void runOpMode() throws InterruptedException
@@ -21,12 +27,23 @@ public class Lamgunana_V1 extends LinearOpMode
 
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
 
+        armServo1 = hardwareMap.servo.get("armServo1");
+        armServo2 = hardwareMap.servo.get("armServo2");
+        gripperServo1 = hardwareMap.servo.get("gripperServo1");
+        gripperServo2 = hardwareMap.servo.get("gripperServo2");
+
         waitForStart();
 
         while(opModeIsActive())
         {
             motorLeft.setPower(-gamepad1.left_stick_y);
             motorRight.setPower(-gamepad1.right_stick_y);
+
+            if(gamepad2.a)
+            {
+                armServo1.setPosition(0.5);
+                armServo2.setPosition(-0.5);
+            }
 
             idle();
         }
