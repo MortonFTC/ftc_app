fe07293391b20a54900bac39cf6564b43e085e51
FIRSTMentor <mortonftc@gmail.com>
2018-05-01 19:54:21 -0500

    updates for fortnite battle royale lol xd

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InovationBot2.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Conveyor Belt Triggers", group = "mortonElements")
+
+public class InovationBot2 extends LinearOpMode
+{
+    private DcMotor motorLeftDrive;
+    private DcMotor motorRightDrive;
+
+    private DcMotor conveyorMotor;
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        motorLeftDrive = hardwareMap.dcMotor.get("motorLeftDrive");
+        motorRightDrive = hardwareMap.dcMotor.get("motorRightDrive");
+
+        conveyorMotor = hardwareMap.dcMotor.get("conveyorMotor");
+        motorLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+
+        waitForStart();
+        while(opModeIsActive())
+        {
+            motorLeftDrive.setPower(-gamepad1.left_stick_y);
+            motorRightDrive.setPower(-gamepad1.right_stick_y);
+
+            conveyorMotor.setPower(-gamepad2.right_stick_y);
+            idle();
+        }
+    }
+}
+
