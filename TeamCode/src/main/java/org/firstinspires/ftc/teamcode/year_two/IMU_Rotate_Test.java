03aa83dcde51bd02099fcfcbe8d2e4202a6729d4
knepjow <mortonftc@gmail.com>
2019-02-08 17:58:23 -0600

    Added class to rotate robot with IMU. More testing needed.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/year_two/IMU_Rotate_Test.java
@@ -0,0 +1,170 @@
+package org.firstinspires.ftc.teamcode.year_two;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous(name = "IMU_Rotate_Test",group = "ProtoRobotics")
+public class IMU_Rotate_Test extends LinearOpMode {
+    DcMotor leftFrontDrive, rightFrontDrive, leftRearDrive, rightRearDrive;
+    BNO055IMU imu;
+    Orientation lastAngles = new Orientation();
+    double globalAngle, power = .30, correction;
+    boolean aButton, bButton, touched;
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftFrontDrive = hardwareMap.dcMotor.get("leftFrontDrive");
+        leftRearDrive = hardwareMap.dcMotor.get("leftRearDrive");
+
+        rightFrontDrive = hardwareMap.dcMotor.get("rightFrontDrive");
+        rightRearDrive = hardwareMap.dcMotor.get("rightRearDrive");
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
+
+        telemetry.addData("Mode", "calibrating...");
+        telemetry.update();
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!isStopRequested() && !imu.isGyroCalibrated()) {
+            sleep(50);
+            idle();
+        }
+
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
+        telemetry.update();
+
+        // wait for start button.
+        waitForStart();
+
+        telemetry.addData("Mode", "running");
+        telemetry.update();
+
+        sleep(1000);
+
+        rotate(90, .5);
+
+        rotate(-180, .5);
+
+        rotate(45, .5);
+    }
+
+    private double getAngle()
+    {
+        // We experimentally determined the Z axis is the axis we want to use for heading angle.
+        // We have to process the angle because the imu works in euler angles so the Z axis is
+        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngles = angles;
+
+        return globalAngle;
+    }
+
+    private void rotate(int degrees, double power)
+    {
+        double  actualDegrees;
+
+        if (degrees > 0)
+        {
+            actualDegrees = degrees -15;
+        }
+        else
+        {
+            actualDegrees = degrees + 15;
+        }
+
+        double  leftPower, rightPower;
+        // restart imu movement tracking.
+        resetAngle();
+
+        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
+        // clockwise (right).
+
+        if (actualDegrees < 0)
+        {   // turn right.
+            leftPower = -power;
+            rightPower = -power;
+        }
+        else if (actualDegrees > 0)
+        {   // turn left.
+            leftPower = power;
+            rightPower = power;
+        }
+        else return;
+
+        // set power to rotate.
+        leftFrontDrive.setPower(leftPower);
+        leftRearDrive.setPower(leftPower);
+
+        rightFrontDrive.setPower(rightPower);
+        rightRearDrive.setPower(rightPower);
+
+        // rotate until turn is completed.
+        if (actualDegrees < 0)
+        {
+            // On right turn we have to get off zero first.
+            while (opModeIsActive() && getAngle() == 0) {}
+
+            while (opModeIsActive() && getAngle() > actualDegrees) {}
+        }
+        else    // left turn.
+            while (opModeIsActive() && getAngle() < actualDegrees) {}
+
+        // turn the motors off.
+        rightFrontDrive.setPower(0);
+        rightRearDrive.setPower(0);
+        leftFrontDrive.setPower(0);
+        leftRearDrive.setPower(0);
+
+        // wait for rotation to stop.
+        sleep(1000);
+
+        // reset angle tracking on new heading.
+        resetAngle();
+    }
+
+    private void resetAngle()
+    {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        globalAngle = 0;
+    }
+}
